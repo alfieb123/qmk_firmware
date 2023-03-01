9e9901898d7c599cda00a93c007bdd9446373746
alfie <alfiebooth@gmail.com>
2023-03-01 19:04:25 +0000

    added custom keymap for ferris sweep (that doesnt work yet for some reason)

---
+++ keyboards/ferris/keymaps/alfieb/rules.mk
@@ -0,0 +1,10 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes    # Mouse keys
+# RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+# OLED_ENABLE     = yes
+# OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+COMBO_ENABLE    = yes
+CAPS_WORD_ENABLE= yes
+# AUTO_SHIFT_ENABLE= yes
