fbaa9bc6ee22f893f292af5e64f9663b827e11d7
alfie <alfiebooth@gmail.com>
2023-08-09 10:49:53 +0100

    made some keymap changes for mac

--- keyboards/ferris/keymaps/alfieb/keymap.c
+++ keyboards/ferris/keymaps/alfieb/keymap.c
@@ -42,7 +42,8 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(c_unds, KC_UNDS), //underscore
     COMBO(c_mins, KC_MINS), //minus
     COMBO(c_eql, KC_EQL), //equal
-    COMBO(c_dquo, KC_AT), // this is a double quote ". notice we are using kc_at which is noramlly the at symbol but not on uk for some reason.
+    //COMBO(c_dquo, KC_AT), // this is a double quote ". notice we are using kc_at which is noramlly the at symbol but not on uk for some reason. ONLY windows and linux!
+    COMBO(c_dquo, KC_DQUO),
    //  COMBO(c_mo3, MO(3)), // layer toggle to layer
    //  COMBO(c_lgui, KC_LGUI), // Os key
    //  COMBO(c_lgui2, KC_LGUI), // Os key
@@ -66,11 +67,13 @@ layer 4, mouse
 #define ALT_KCS LALT_T(KC_S)
 #define CTL_KCD LCTL_T(KC_D)
 #define SFT_KCF LSFT_T(KC_F)
+#define GUI_KCG LGUI_T(KC_G)
 
 #define GUI_SCN LGUI_T(KC_SCLN)
 #define ALT_KCL LALT_T(KC_L)
 #define CTL_KCK LCTL_T(KC_K)
 #define SFT_KCJ RSFT_T(KC_J)
+#define GUI_KCH LGUI_T(KC_H)
 
 // #define GUI_NUS LGUI_T(KC_NUHS)
 // #define ALT_DLR LALT_T(KC_DLR)
@@ -86,6 +89,7 @@ layer 4, mouse
 #define ALT_NU6 LALT_T(KC_6)
 #define CTL_NU5 LCTL_T(KC_5)
 #define SFT_NU4 LSFT_T(KC_4)
+#define GUI_DOT LGUI_T(KC_DOT)
 
 #define GUI__F8 LGUI_T(KC_F8)
 #define ALT__F7 LALT_T(KC_F7)
@@ -100,6 +104,11 @@ layer 4, mouse
 #define LT3_KCR LT(3, KC_R)
 #define LT4_KCT LT(4, KC_T)
 
+// mac virtual desktops
+#define MVD_NXT LCTL(KC_RGHT)
+#define MVD_PRV LCTL(KC_LEFT)
+
+// windows virtual desktops
 #define WVD_NXT LGUI(LCTL(KC_RGHT))
 #define WVD_PRV LGUI(LCTL(KC_LEFT))
 #define WNCLOSE LALT(KC_F4)
@@ -107,7 +116,7 @@ layer 4, mouse
 // lgui lalt lsft, this has been put here specificlly for the the windows store app
 // 'Virtual Desktrop Indictor', that has number based switching desktops with win alt shift num,
 // and switching windows to numbered virtual desktop with win alt shift Fnum (so, F2 instead of 2)
-#define GAS LGUI(LALT(KC_LSFT))
+//#define GAS LGUI(LALT(KC_LSFT))
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -115,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------.                    ,---------------------------------------------
         KC_Q,    KC_W,    KC_E, LT3_KCR, LT4_KCT,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
   //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+
-     GUI_KCA, ALT_KCS, CTL_KCD, SFT_KCF,    KC_G,                         KC_H, SFT_KCJ, CTL_KCK, ALT_KCL, GUI_SCN,
+        KC_A, ALT_KCS, CTL_KCD, SFT_KCF, GUI_KCG,                      GUI_KCH, SFT_KCJ, CTL_KCK, ALT_KCL, KC_SCLN,
   //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
   //|-------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+
@@ -141,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------.                    ,---------------------------------------------
      KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR,KC_TILDE,                      _______, KC_CIRC, KC_MINS, KC_QUOT, _______,
   //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+
-     KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN, KC_PIPE,                      _______,  KC_EQL, KC_UNDS, KC_DQUO, KC_LGUI,
+     KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN, KC_PIPE,                      KC_LGUI,  KC_EQL, KC_UNDS, KC_DQUO, KC_LGUI,
   //|-------+--------+--------+--------+--------j                    |--------+--------+--------+--------+--------+
      KC_PERC, KC_HASH, KC_LBRC, KC_RBRC, KC_AMPR,                      _______, KC_BSLS, _______, _______, _______,
   //|-------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+
@@ -151,13 +160,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT(
   //,-------------------------------------------.                    ,---------------------------------------------
-      KC_F12,  KC_F11,  KC_F10,   KC_F9, WVD_NXT,                       KC_EQL,    KC_7,    KC_8,    KC_9, KC_PAST,
+      KC_F12,  KC_F11,  KC_F10,   KC_F9, MVD_NXT,                       KC_EQL,    KC_7,    KC_8,    KC_9, KC_PAST,
   //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+
-     GUI__F8, ALT__F7, CTL__F6, SFT__F5, WVD_PRV,                       KC_DOT, SFT_NU4, CTL_NU5, ALT_NU6, GUI_PLS,
+       KC_F8, ALT__F7, CTL__F6, SFT__F5, KC_LGUI,                      GUI_DOT, SFT_NU4, CTL_NU5, ALT_NU6, KC_PPLS,
   //|-------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+
-       KC_F4,   KC_F3,   KC_F2,   KC_F1,     GAS,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PMNS,
+       KC_F4,   KC_F3,   KC_F2,   KC_F1, MVD_PRV,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PMNS,
   //|-------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+
-                                             GAS, _______,    _______,   KC_DEL
+                                         _______, _______,    _______,   KC_DEL
                                       //-----------------'  `------------------
   ),
 
