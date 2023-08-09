1c10032a87498fdf01ba4a89fd12560bb3654ef2
alfie <alfiebooth@gmail.com>
2023-08-09 09:08:45 +0100

    modified the compile and edit scripts back to linux and new location

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 qmk compile -kb ferris/sweep_choc_mbuk -km alfieb
 
 #for wsl, open the directory here so that we can drop it into qmk toolbox
-explorer.exe .
+#explorer.exe .
 
-/mnt/c/Program\ Files/QMK\ Toolbox/qmk_toolbox.exe &
+#/mnt/c/Program\ Files/QMK\ Toolbox/qmk_toolbox.exe &
--- edit.sh
+++ edit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-code ~/qmk_firmware/keyboards/ferris/keymaps/alfieb &
+# code ~/qmk_firmware/keyboards/ferris/keymaps/alfieb &
+nvim /mnt/mydata/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb &
