707266c18c6e5671b12823e9876022ee1e3f12f9
alfie <alfiebooth@gmail.com>
2023-11-16 09:34:42 +0000

    renamed the sweep stuff

--- edit-sweep.sh
+++ edit-sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nvim /home/parallels/qmk_firmware/keyboards/ferris/keymaps/alfieb
+hx /Users/alfie/Documents/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb
 # nvim /mnt/mydata/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb &
 # code ~/qmk_firmware/keyboards/ferris/keymaps/alfieb &
