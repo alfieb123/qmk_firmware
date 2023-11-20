a4de884264b96d408835616aa5c11b8cb9b4963a
alfie <alfiebooth@gmail.com>
2023-11-20 13:10:23 +0000

    keymap change for sweeps

--- keyboards/ferris/keymaps/alfieb/config.h
+++ keyboards/ferris/keymaps/alfieb/config.h
@@ -18,12 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // #define MASTER_LEFT
 
 #define COMBO_COUNT 7
-#define COMBO_TERM 30 
+#define COMBO_TERM 30
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
