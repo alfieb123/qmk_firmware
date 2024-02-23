fb21c0941c52bff9695c2278b425874cddbb5342
alfie <alfiebooth@gmail.com>
2024-02-23 06:49:57 +0000

    made the edit scripts os agnostic

--- edit-sweep.sh
+++ edit-sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-hx /Users/alfie/Documents/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb
+hx $HOME/Documents/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb
 # nvim /mnt/mydata/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb &
 # code ~/qmk_firmware/keyboards/ferris/keymaps/alfieb &
--- edit-tbk.sh
+++ edit-tbk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-hx /Users/alfie/Documents/_REPOSITORIES_/qmk_firmware/keyboards/bastardkb/tbkmini 
+hx $HOME/Documents/_REPOSITORIES_/qmk_firmware/keyboards/bastardkb/tbkmini 
 # nvim /mnt/mydata/_REPOSITORIES_/qmk_firmware/keyboards/ferris/keymaps/alfieb &
 # code ~/qmk_firmware/keyboards/ferris/keymaps/alfieb &
